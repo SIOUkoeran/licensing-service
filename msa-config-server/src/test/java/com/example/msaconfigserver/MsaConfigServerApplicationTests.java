b9ffdeda08c2268fba60d41bdbcde0dedcb51229
SIOUkoeran <alstnalsgud@gmail.com>
2022-05-21 18:07:36 +0900

    Add Config-server Initial

---
+++ msa-config-server/src/test/java/com/example/msaconfigserver/MsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
